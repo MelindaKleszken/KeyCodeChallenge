b2bb228271c143e043939ac3b894ad3e6196ca65
MelindaKleszken <melindakleszken1021@gmail.com>
2020-12-15 18:35:44 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const keyValueBtn = document.querySelector("#keyValue");
+const keyCodeBtn = document.querySelector("#keyCode");
+const keyCharBtn = document.querySelector("#charCode");
+const main = document.querySelector("#heading");
+
+document.addEventListener("keydown", (event) => {     
+    keyValueBtn.innerHTML = event.key;
+    keyCodeBtn.innerHTML = event.code;
+    keyCharBtn.innerHTML = event.which; 
+    main.innerHTML = event.key;
+})
